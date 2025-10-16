2c8eac3adb47531b90319b2232410eed11cddbc9
fakhreddinelargou <largofakhrddin@gmail.com>
2025-10-16 12:57:12 +0100

    intiall

---
+++ about/about.js
@@ -0,0 +1,25 @@
+
+
+// const AllCards = document.querySelector(".AllCards")
+// const comenters = document.querySelector(".comenters")
+// const cmts = document.querySelectorAll(".cmts")
+// // const cmt2 = document.querySelector(".cmt-2")
+// // const cmt3 = document.querySelector(".cmt-3")
+
+// console.log(AllCards, comenters, cmts);
+
+
+// let cmtIndex = 1 ;
+// let interValid;
+
+
+// const autoSlide = ()=>{
+//     interValid = setInterval(()=> slideCmt(++cmtIndex),2000)
+// }
+// autoSlide()
+// const slideCmt = ()=>{
+//     console.log(cmtIndex)
+//     comenters.style.transform = `translate(-${cmtIndex * 100})`
+// }
+
+// // slideCmt()
